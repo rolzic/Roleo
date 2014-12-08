6c6643528282a2a14a8fcc5ce82efe70a8ccf5db
RolandR <roland.reinvald@gmail.com>
2014-12-08 14:42:16 +0200

    siin me muudame programmi nigu nalja

--- Roleo.py
+++ Roleo.py
@@ -1,45 +1,66 @@
 from timeit import default_timer as timer
-nimi = input("Mis on sinu nimi? ")
-print("Sisesta siia laul või luuletus: ")
-print("Kui saad luuletuse sisestatud, vajuta kaks korda enter")
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import font
+
+
+def roleo():
+    nimi = input("Mis on sinu nimi? ")
+    print("Sisesta siia laul või luuletus: ")
+    print("Kui saad luuletuse sisestatud, vajuta kaks korda ENTERIT")
 
 
 
 
-buffer = []
-while True:
-    line = input()
-    if line == "":
-        break
-    buffer.append(line)
-a = "\n".join(buffer)
-
-
-
-
-a = a.replace(",","").replace(".","").replace("!","").replace(":","")\
-    .replace("(","").replace(")","").replace("-","").replace(";","")
-
-start = timer()
-a = a.split("\n")
-k = 0
-while True:
-    b = input("Sisesta " + str(k+1)+ " " + "rida: ")
-    b = b.replace(",","").replace(".","").replace("!","").replace(":","")\
-    .replace("(","").replace(")","").replace("-","").replace(";","")
-    if a[k].lower() == b.lower():
-        b = ""
-        k += 1
-        if k == len(a):
-            print("Hästi tehtud,", nimi)
+    buffer = []
+    while True:
+        line = input()
+        if line == "":
             break
-        print("Tubli!")
-    else:
-        print("Vale")
-        print("Õige on:",a[k])
-        print("Ära muretse, proovi uuesti")
-        k = k
-end = timer()
-print("Sul läks aega " + str(round(end - start, 2)) + " sekundit")
+        buffer.append(line)
+    a = "\n".join(buffer)
+
+
+
+
+    a = a.replace(",","").replace(".","").replace("!","").replace(":","")\
+        .replace("(","").replace(")","").replace("-","").replace(";","")
+
+    start = timer()
+    a = a.split("\n")
+    k = 0
+    while True:
+        b = input("Sisesta " + str(k+1)+ " " + "rida: ")
+        b = b.replace(",","").replace(".","").replace("!","").replace(":","")\
+        .replace("(","").replace(")","").replace("-","").replace(";","")
+        if a[k].lower() == b.lower():
+            b = ""
+            k += 1
+            if k == len(a):
+                print("Hästi tehtud,", nimi)
+                break
+            print("Tubli!")
+        else:
+            print("Vale")
+            print("Õige on:",a[k])
+            print("Proovi uuesti")
+            k = k
+    end = timer()
+    print("Sul läks aega " + str(round(end - start, 2)) + " sekundit")
+
+raam = Tk()
+raam.title("Roleo")
+tahvel = Canvas(raam, width=1000, height=1000, background="SkyBlue")
+tahvel.grid()
+
+suur_font = font.Font(family='Helvetica', size=32, weight='bold')
+tahvel.create_text(30, 500, text="Oled valmis luuletust õppima?", font=suur_font, anchor=NW)
+
+nupp = ttk.Button(raam, text="Alusta luuletuse õppimist!", command=roleo)
+nupp.place(x=70, y=40, width=150)
+
+
+raam.mainloop()
 
 
