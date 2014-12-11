5517b567ef2c53c65a45d617ee900df3be613331
RolandR <roland.reinvald@gmail.com>
2014-12-11 14:59:32 +0200

    siin me muudame programmi nigu nalja

--- Roleo.py
+++ Roleo.py
@@ -3,80 +3,294 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import sys
-
-def luuletus():
-    a = t.get(1.0,END)
-    a = str(a.strip())
-    a = a.replace(",","").replace(".","").replace("!","").replace(":","")\
-    .replace("(","").replace(")","").replace("-","").replace(";","")
-    a = a.lower()
-    a = a.split("\n")
-    print(a)
-    raam.destroy()
-    def luuletus2():
-
-        global i
-        global j
-        b = c.get()
-        b = str(b.strip())
-        b = b.replace(",","").replace(".","").replace("!","").replace(":","")\
+def õp():
+    valik.destroy()
+    def luuletus():
+        nimi = d.get()
+        a = t.get(1.0,END)
+        a = str(a.strip())
+        a = a.replace(",","").replace(".","").replace("!","").replace(":","")\
         .replace("(","").replace(")","").replace("-","").replace(";","")
-        b = b.lower()
-        print(b)
-        if a[i] == b:
-            messagebox.showinfo(message="Väga tubli")
-            c.delete(0, END)
-            if i+1 == len(a):
-                end = timer()
-                messagebox.showinfo(message="Vigade arv: " + str(j) + "." \
-                + " Sul läks aega  " + str(round(end - start, 2)) + " sekundit.")
-                raam2.destroy()
-
+        a = a.lower()
+        a = a.split("\n")
+        print(a)
+        print(nimi)
+        raam.destroy()
+        def luuletus2():
+            global i_e
+            global j_e
+            global g_e
+            global uus_e
+            b = c.get()
+            b = str(b.strip())
+            b = b.replace(",","").replace(".","").replace("!","").replace(":","")\
+            .replace("(","").replace(")","").replace("-","").replace(";","")
+            b = b.lower()
+            print(b)
+            if a[i_e] == b:
+                messagebox.showinfo(message="Väga tubli")
+                c.delete(0, END)
+                if i_e+1 == len(a):
+                    end = timer()
+                    h = str(round(end - start, 2))
+                    messagebox.showinfo(message="Vigade arv: " + str(j_e) + "." + " Sul läks aega: " + h + " sekundit.")
+                    raam2.destroy()
+                    f = open(nimi +"_educational" + ".txt","w")
+                    f.write("Vigu: " + str(j_e) + "\n")
+                    f.write("Läks aega: " +h + "\n")
+                    f.write("Tekst: " + "\n")
+                    f.write("\n".join(a))
+                    f.close()
+                else:
+                    i_e+=1
+                    g_e = 0
+                    uus_e = []
             else:
-                i+=1
-        else:
-            messagebox.showinfo(message="Läks valesti, aga pole hullu,proovime uuesti!")
-            c.delete(0, END)
-            j += 1
-    raam2 = Tk()
-    raam2.title("Luuletuse meeldejätmine")
-    tahvel = Canvas(raam2, width=600, height=600, background="black")
+                lisa = a[i_e].split()
+                pikkus = len(lisa)
+                if g_e+1 == len(lisa):
+                    messagebox.showinfo(message="Siin on sinu rida, proovi see meelde jätta: " + str(a[i_e]))
+                    c.delete(0, END)
+                    j_e += 1
+                    g_e = g_e
+                else:
+                    uus_e += [lisa[g_e]]
+                    messagebox.showinfo(message="Läks valesti, aga pole hullu. Proovi uuesti!")
+                    messagebox.showinfo(message="Siin on väike vihje: " + str(" ".join(uus_e)))
+                    c.delete(0, END)
+                    j_e += 1
+                    g_e +=1
+        raam2 = Tk()
+        raam2.title("Luuletuse meeldejätmine")
+        raam2.bind("<Return>", lambda event: luuletus2())
+        tahvel3 = Canvas(raam2, width=300, height=500)
+        tahvel3.grid()
+        nupp2 = ttk.Button(tahvel3,text = "OK",command = luuletus2)
+        nupp2.place(x=30,y=60)
+        start = timer()
+        c = Entry(tahvel3)
+        c.pack()
+        c.place(x=30,y=30,width=240)
+        edu = PhotoImage(file="edu.gif")
+        img3 = tahvel3.create_image(160, 300, image=edu)
+        tahvel3.pack()
+        raam2.mainloop()
+
+
+    raam = Tk()
+    raam.title("Luuletuse sisestamine")
+    tahvel2 = Canvas(raam, width=900, height=700, background="white")
+    tahvel2.grid()
+    nupp = ttk.Button(tahvel2,text = "OK",command = luuletus)
+    nupp.place(x=30,y=660)
+    d = Entry(tahvel2)
+    d.pack()
+    d.place(x = 70,y=50, width= 200)
+    t = Text(tahvel2, width=30,height=35, background="black", fg="white")
+    t.place(x=30,y=80)
+    silt = ttk.Label(tahvel2, text="Nimi", background= "white")
+    silt.place(x=30, y=50)
+
+    nelson = PhotoImage(file="nelson.gif")
+    img = tahvel2.create_image(580, 490, image=nelson)
+    jutumull = PhotoImage(file="jutumull.gif")
+    img2 = tahvel2.create_image(580, 180, image=jutumull)
+    tahvel2.pack()
+    raam.mainloop()
+
+def män():
+    valik.destroy()
+    def luuletus():
+        player1 = d.get()
+        player2 = v.get()
+        a = t.get(1.0,END)
+        a = str(a.strip())
+        a = a.replace(",","").replace(".","").replace("!","").replace(":","")\
+        .replace("(","").replace(")","").replace("-","").replace(";","")
+        a = a.lower()
+        a = a.split("\n")
+        print(a)
+        raam.destroy()
+        def luuletus2():
+            global i_m1
+            global j_m1
+            global g_m1
+            global uus_m1
+            esimene = c.get()
+            esimene = str(esimene.strip())
+            esimene = esimene.replace(",","").replace(".","").replace("!","").replace(":","")\
+            .replace("(","").replace(")","").replace("-","").replace(";","")
+            esimene = esimene.lower()
+            print(esimene)
+            if a[i_m1] == esimene:
+                messagebox.showinfo(message="Väga tubli")
+                c.delete(0, END)
+                if i_m1+1 == len(a):
+                    end1 = timer()
+                    h = str(round(end1 - start1, 2))
+                    messagebox.showinfo(message="Vigade arv: " + str(j_m1) + "." + " Sul läks aega " + h + " sekundit.")
+                    raam2.destroy()
+                    f = open(player1 + "_1player" + ".txt","w")
+                    f.write("Vigu: " + str(j_m1) + "\n")
+                    f.write("Läks aega: " +h + "\n")
+                    f.close()
+                    def luuletus3():
+                        global i_m2
+                        global j_m2
+                        global g_m2
+                        global uus_m2
+                        teine = z.get()
+                        teine = str(teine.strip())
+                        teine = teine.replace(",","").replace(".","").replace("!","").replace(":","")\
+                        .replace("(","").replace(")","").replace("-","").replace(";","")
+                        teine = teine.lower()
+                        print(teine)
+                        if a[i_m2] == teine:
+                            messagebox.showinfo(message="Väga tubli")
+                            z.delete(0, END)
+                            if i_m2+1 == len(a):
+                                end2 = timer()
+                                x = str(round(end2 - start2, 2))
+                                messagebox.showinfo(message="Tegite vigu: " + str(j_m2) + "." + " Sul läks aega " + x + " sekundit.")
+                                f = open(player2 +"_2player" + ".txt","w")
+                                f.write("Vigu: " + str(j_m2) + "\n")
+                                f.write("Läks aega: " +x + "\n")
+                                f.close()
+                                if x < h:
+                                    messagebox.showinfo(message="2. mängija oli kiirem")
+                                elif x ==h:
+                                    messagebox.showinfo(message="Mõlemad mängijad olid sama kiired")
+                                else:
+                                    messagebox.showinfo(message="1. mängija oli kiirem")
+                                if j_m1 > j_m2:
+                                    messagebox.showinfo(message="1. mängijal on rohkem vigu")
+                                    raam3.destroy()
+                                elif j_m1 == j_m2:
+                                    messagebox.showinfo(message="Mõlemal oli sama palju vigu")
+                                    raam3.destroy()
+                                else:
+                                    messagebox.showinfo(message="1. mängijal on vähem vigu")
+                                    raam3.destroy()
+                            else:
+                                i_m2+=1
+                                g_m2 = 0
+                                uus_m2 = []
+                        else:
+                            lisa2 = a[i_m2].split()
+                            pikkus = len(lisa2)
+                            if g_m2+1 == len(lisa2):
+                                messagebox.showinfo(message="Siin on sinu rida, proovi see meelde jätta: " + str(a[i_m2]))
+                                z.delete(0, END)
+                                j_m2 += 1
+                                g_m2 = g_m2
+                            else:
+                                uus_m2 += [lisa2[g_m2]]
+                                messagebox.showinfo(message="Läks valesti, aga pole hullu. Proovi uuesti!")
+                                messagebox.showinfo(message="Siin on väike vihje: " + str(" ".join(uus_m2)))
+                                z.delete(0, END)
+                                j_m2 += 1
+                                g_m2 +=1
+                    raam3 = Tk()
+                    raam3.title("2. player")
+                    raam3.bind("<Return>", lambda event: luuletus3())
+                    tahvel4 = Canvas(raam3, width=300, height=500)
+                    tahvel4.grid()
+                    nupp3 = ttk.Button(tahvel4,text = "OK",command = luuletus3)
+                    nupp3.place(x=30,y=60)
+                    start2 = timer()
+                    z = Entry(tahvel4)
+                    z.pack()
+                    z.place(x=30,y=30,width=240)
+                    player2foto = PhotoImage(file="player2.gif")
+                    img3 = tahvel4.create_image(160, 300, image=player2foto)
+                    tahvel4.pack()
+                    raam3.mainloop()
+                else:
+                    i_m1+=1
+                    g_m1 = 0
+                    uus_m1 = []
+            else:
+                lisa = a[i_m1].split()
+                pikkus = len(lisa)
+                if g_m1+1 == len(lisa):
+                    messagebox.showinfo(message="Siin on sinu rida, proovi see meelde jätta: " + str(a[i_m1]))
+                    c.delete(0, END)
+                    j_m1 += 1
+                    g_m1 = g_m1
+                else:
+                    uus_m1 += [lisa[g_m1]]
+                    messagebox.showinfo(message="Läks valesti, aga pole hullu. Proovi uuesti!")
+                    messagebox.showinfo(message="Siin on väike vihje: " + str(" ".join(uus_m1)))
+                    c.delete(0, END)
+                    j_m1 += 1
+                    g_m1 +=1
+        raam2 = Tk()
+        raam2.title("1. player")
+        raam2.bind("<Return>", lambda event: luuletus2())
+        canvas = Canvas(raam2, width=300, height=500)
+        canvas.grid()
+        nupp2 = ttk.Button(canvas,text = "OK",command = luuletus2)
+        nupp2.place(x=30,y=60)
+        start1 = timer()
+        c = Entry(canvas)
+        c.pack()
+        c.place(x=30,y=30,width=240)
+        player1foto = PhotoImage(file="player1.gif")
+        img3 = canvas.create_image(160, 300, image=player1foto)
+        canvas.pack()
+        canvas.mainloop()
+    raam = Tk()
+    raam.title("Luuletuse sisestamine")
+    tahvel = Canvas(raam, width=900, height=700, background="white")
     tahvel.grid()
-    nupp2 = Button(tahvel,text = "OK",command = luuletus2, fg="black",bg="SkyBlue").pack()
-    c = Entry(tahvel)
-    c.pack()
-    start = timer()
+    nupp = ttk.Button(tahvel,text = "OK",command = luuletus)
+    nupp.place(x=30, y=670)
+    d = Entry(tahvel)
+    d.pack()
+    d.place(x = 100,y=50, width=150)
+    v = Entry(tahvel)
+    v.pack()
+    v.place(x = 100, y=70, width=150)
+    silt1 = ttk.Label(tahvel, text="Player 1", background="white")
+    silt1.place(x=30,y=50)
 
+    silt2 = ttk.Label(tahvel, text="Player 2", background="white")
+    silt2.place(x=30,y=70)
+    t = Text(tahvel, width=30,height=35, background="black", fg="white")
+    t.place(x=30,y=100)
 
-i = 0
-j = 0
+    nelson = PhotoImage(file="nelson.gif")
+    img = tahvel.create_image(580, 490, image=nelson)
+    jutumull = PhotoImage(file="jutumull.gif")
+    img2 = tahvel.create_image(580, 180, image=jutumull)
+    tahvel.pack()
+    raam.mainloop()
 
-#Esimene raam
-raam = Tk()
-raam.title("Luuletuse sisestamine")
-raam.geometry("800x800")
+uus_e =  []
+g_e = 0
+i_e = 0
+j_e = 0
+uus_m1 =  []
+g_m1 = 0
+i_m1 = 0
+j_m1 = 0
+uus_m2 =  []
+g_m2 = 0
+i_m2 = 0
+j_m2 = 0
+valik = Tk()
+valik.title("Vali mode")
+tahvel = Canvas(valik, width=450, height=452, background="black")
+tahvel.grid()
+educational = Button(tahvel,text = "Educational",command = õp, fg ="black", bg="yellow", padx=30,pady=30)
+educational.pack()
+educational.place(x= 90,y=45)
+mäng = Button(tahvel,text = "Mäng",command=män, fg ="black",bg="yellow", padx=46,pady=30)
+mäng.pack()
+mäng.place(x=90,y=150)
 
-#Esimese raami nupp
-nupp = ttk.Button(raam,text = "OK",command = luuletus)
-nupp.place(x=400, y=370, width=70)
-
-#Esimese raami tekstikast
-t = Text(raam, width=40,height=20)
-t.place(x=400,y=30)
-
-#Käsnakalle foto
-photo = PhotoImage(file='pilt2.gif')
-label= Label(raam, image=photo)
-label.place(x=30, y=20)
-
-#Jutumull
-photo2 = PhotoImage(file='pilt3.gif')
-label2= Label(raam, image=photo2)
-label2.place(x=320, y=400)
-
-#Nupu style
-s = ttk.Style()
-s.theme_use("vista")
-raam.mainloop()
+photo = PhotoImage(file='mustpilt.gif')
+label= Label(tahvel, image=photo, background="black")
+label.place(x=240, y=2)
 
 
